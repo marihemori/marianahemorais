b7dec44ebb779ff07c8fecf6140c30fae1d542a8
Mariana <moraismariana83@gmail.com>
2023-08-31 16:58:57 -0300

    Fix typescript type

--- components/Particles.tsx
+++ components/Particles.tsx
@@ -21,7 +21,7 @@ export default function Particles({
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const canvasContainerRef = useRef<HTMLDivElement>(null)
   const context = useRef<CanvasRenderingContext2D | null>(null)
-  const circles = useRef<any[]>([])
+  const circles = useRef<never[]>([])
   const mousePosition = useMousePosition()
   const mouse = useRef<{ x: number; y: number }>({ x: 0, y: 0 })
   const canvasSize = useRef<{ w: number; h: number }>({ w: 0, h: 0 })
